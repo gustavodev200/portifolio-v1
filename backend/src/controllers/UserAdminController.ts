03fee69ba3ee33d4d3feae08bae3ee61a34bf870
gustavodev200 <gustavolage200@gmail.com>
2022-05-11 22:57:10 -0300

    created route to admin and password encrypted added password encrypted

--- backend/src/controllers/UserAdminController.ts
+++ backend/src/controllers/UserAdminController.ts
@@ -1,16 +1,58 @@
 import { Request, Response } from "express";
 import { UserAdmin } from "../models/UserAdmin";
+import bcrypt from "bcrypt";
+
+interface IUserAdmin {
+  name: string;
+  email: string;
+  password: string;
+}
 
 class UserAdminController {
-  async create(req: Request, res: Response) {
-    const { name, email, password } = req.body;
+  // async create(req: Request, res: Response) {
+  //   const { name, email, password }: IUserAdmin = req.body;
+  //   const salt = await bcrypt.genSalt(12);
+  //   const passwordHash = await bcrypt.hash(password, salt);
+  //   const user = await UserAdmin.create({
+  //     email,
+  //     name,
+  //     password: passwordHash,
+  //   });
+  //   return res.status(201).json(user);
+  // }
 
-    const user = await UserAdmin.create({
-      email,
-      name,
-      password,
-    });
-    return res.status(201).json(user);
+  async login(req: Request, res: Response) {
+    const { email, password }: IUserAdmin = req.body;
+
+    if (!email) {
+      res.status(422).json({ message: "O e-mail é obrigatório!" });
+      return;
+    }
+
+    if (!password) {
+      res.status(422).json({ message: "A senha é obrigatória!" });
+      return;
+    }
+
+    const user: any = await UserAdmin.findOne({ where: { email: email } });
+
+    if (!user) {
+      res
+        .status(422)
+        .json({ message: "Não há usuário cadastrado com esse e-mail!" });
+      return;
+    }
+
+    const checkPassword: string = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword) {
+      res.status(422).json({ message: "Senha inválida" });
+      return;
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Administrador logado com sucesso!", user });
   }
 }
 
