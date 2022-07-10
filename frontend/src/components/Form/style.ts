c3208311cbf5c86792d2cbbc2f0778fc19946790
gustavodev200 <gustavolage200@gmail.com>
2022-07-10 02:10:26 -0300

    starting contact page

---
+++ frontend/src/components/Form/style.ts
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+import { device } from "../../styles/responsive";
+import pixelToRem from "../../utils/pxToRem";
--- frontend/src/pages/Contact/index.tsx
+++ frontend/src/pages/Contact/index.tsx
@@ -1,9 +1,20 @@
+import Form from "../../components/Form";
 import Title from "../../components/Title";
+import { useForm } from "react-hook-form";
 
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   return (
     <section>
       <Title firstText="CONTATO" secondText="GUSTAVO" />
+      <div>
+        {/* <Form type="text" name="username" placeholder="Nome" register="name" /> */}
+      </div>
     </section>
   );
 };
