6d51329c50c00e418d90e2fc8dd647058578a8af
gustavodev200 <gustavolage200@gmail.com>
2022-07-10 21:07:55 -0300

    entrando na parte de tela admin

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,11 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import AdminPage from "./pages/AdminPage";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ frontend/src/pages/AdminPage/index.tsx
@@ -0,0 +1,11 @@
+import { AdminPageWrapper } from "./style";
+
+const AdminPage = () => {
+  return (
+    <AdminPageWrapper>
+      <h1>oi</h1>
+    </AdminPageWrapper>
+  );
+};
+
+export default AdminPage;
